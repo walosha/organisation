b2d4bb61a4c20a8519a035d70e49d748ed91cee1
olawale afuye <walosha@yahoo.com>
2020-08-11 14:06:00 +0100

    fixed textimony grid bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f345bed7d8f44d3b51ab";
+/******/ 	var hotCurrentHash = "c229701dc7986aa9d0d6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
