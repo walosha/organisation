fd2e35887763556bf38f9f87ac4a1ee89f37e670
Olawale Afuye <walosha@yahoo.com>
2022-01-19 22:30:08 +0100

    fix

--- src/api/contentful.js
+++ src/api/contentful.js
@@ -1,1 +1,13 @@
+import contentful from "contentful";
 
+const client = contentful.createClient({
+  space: "0jomzuod9ed3",
+  accessToken: "NUgr0P91NskCprP60jTIF9vIiwadKcPtRX-cFKHDufY",
+});
+
+async function getPeople() {
+  const entries = await client.getEntries({
+    content_type: "Pacific_Professional",
+  });
+  return entries.items;
+}
