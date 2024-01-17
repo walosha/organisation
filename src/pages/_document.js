257eb9902cd801b4ea136b6d2b64557c483aa346
olawale afuye <walosha@yahoo.com>
2024-01-17 15:40:02 +0100

    added chat bot

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,45 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Chat Bots for App */}
+
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `
+                 (function collectId(w, d) { 
+                  w.CollectId = "6008d3eadd5fd55abf04ac52";
+                  var h = d.head || d.getElementsByTagName("head")[0]; 
+                  var s = d.createElement("script"); s.setAttribute("type", "text/javascript");
+                  s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js");
+                  h.appendChild(s); })(window, document)
+            `,
+          }}
+        />
+        {/* Global site tag (gtag.js) - Google Analytics */}
+
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag() {
+                    dataLayer.push(arguments);
+                  }
+                  gtag("js", new Date());
+              
+                  gtag("config", "UA-131379491-2");
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
